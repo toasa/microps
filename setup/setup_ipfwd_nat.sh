726d0fc4b737fe8f720279ea1fa01da98180259c
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2025-03-01 13:43:03 +0900

    add setup script for ip forwarding and NAT setting

---
+++ setup/setup_ipfwd_nat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+sudo bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo iptables -A FORWARD -o tap0 -j ACCEPT
+sudo iptables -A FORWARD -i tap0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -s 192.0.2.0/24 -o eth0 -j MASQUERADE
